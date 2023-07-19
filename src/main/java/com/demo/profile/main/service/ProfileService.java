2689bd99a6d1f5458c6c1d9ef581e7ff4711c36b
Carlos <carlos@dspot.com.pl>
2023-07-19 13:30:03 +0200

    Add endpoint to get a Profile using a given id.

--- src/main/java/com/demo/profile/main/service/ProfileService.java
+++ src/main/java/com/demo/profile/main/service/ProfileService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ProfileService {
@@ -17,8 +18,17 @@ public class ProfileService {
         this.profileRepository = profileRepository;
     }
 
-
     public List<Profile> getAllProfiles() {
         return profileRepository.findAll();
+    }
+
+    public Profile getProfile(Long id) {
+        Optional<Profile> profileOptional = profileRepository.findById(id);
+
+        if (profileOptional.isPresent()) {
+            return profileOptional.get();
+        }
+
+        throw new IllegalStateException("Profile with id " + id + " does not exists");
     }
 }
